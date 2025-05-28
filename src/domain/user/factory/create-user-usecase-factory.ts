167b4a0ac0dd83e210af0f8da31dfd84389475da
Mauricio Cardoso <mauricio.cds00@gmail.com>
2025-05-28 15:46:23 -0300

    fix: fix import

--- src/domain/user/factory/create-user-usecase-factory.ts
+++ src/domain/user/factory/create-user-usecase-factory.ts
@@ -1,6 +1,6 @@
 import { bCryptAdapterFactory } from "../../../infra/bcrypt/factory";
 import { userRepositoryFactory } from "../repository/user-repository-factory";
-import { CreateUserUsecase } from "../usecase/create-user-usecase";
+import { CreateUserUsecase } from "../usecase/create-user-use-case";
 
 export function createUserUsecaseFactory(): CreateUserUsecase {
   return CreateUserUsecase.getInstance(
